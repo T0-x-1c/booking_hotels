273389ac02ff18bb6e3bcba34ef8dd9827105b93
sem_naz <semerenkonazar90@gmail.com>
2025-06-28 12:30:05 +0300

    adding login, logout, register

--- booking/settings.py
+++ booking/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hotel_system'
+    'hotel_system',
+    'auth_system',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kyiv'
 
 USE_I18N = True
 
